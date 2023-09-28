8a2cf1e44cc00a3095bdc6a7733c8a606f8e218e
UsuarioM <espacioguriats@gmail.com>
2023-09-28 14:01:56 +0200

    React Router

--- reactrouter/src/main.jsx
+++ reactrouter/src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+
+import Home from "./pages/Home/Home.jsx"
+import About from "./pages/About/About.jsx"
+import Heroes from "./pages/Heroes/Heroes.jsx"
+import Heroe from "./pages/Heroe/Heroe.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
